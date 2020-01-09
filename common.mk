d0d957cb6e151f3e1ea392118c648a0d2f01ff7a
Arvind Kumar <karvin@codeaurora.org>
2020-01-09 12:08:57 +0530

    a50-common: Enable graphics mapper version 2.1
     Enable graphics mapper version 2.1
    
    Change-Id: Iad70754a980eadcfd68752c76e569b503d398fae
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Signed-off-by: santhosh6194 <santhosh6194@gmail.com>

--- common.mk
+++ common.mk
@@ -81,8 +81,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.2-service \
-    android.hardware.graphics.mapper@2.0-impl
-    
+    android.hardware.graphics.mapper@2.0-impl-2.1
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
