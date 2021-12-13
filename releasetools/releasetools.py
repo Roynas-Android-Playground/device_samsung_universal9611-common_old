79957c9f6e9b3bd6ab5196f0700df83ac932ac11
santhosh6194 <santhosh6194@gmail.com>
2021-12-13 17:24:21 +0530

    m30s-common: mistyped VBMETA instead of IMAGES in previous commit
    Signed-off-by: santhosh6194 <santhosh6194@gmail.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -37,5 +37,5 @@ def PrintInfo(info, dest):
 def OTA_InstallEnd(info):
   AddImage(info, "RADIO", "dtb.img", "/dev/block/by-name/dtb")
   AddImage(info, "IMAGES", "dtbo.img", "/dev/block/by-name/dtbo")
-  AddImage(info, "VBMETA", "vbmeta.img", "/dev/block/by-name/vbmeta")
+  AddImage(info, "IMAGES", "vbmeta.img", "/dev/block/by-name/vbmeta")
   return
