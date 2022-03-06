a4bc26090e480701a991c18674127e5284f1a0b7
Даня <48557356+fakelog@users.noreply.github.com>
2022-03-06 12:58:55 +0000

    now uses GCameraGoPrebuilt instead of Snap

--- common.mk
+++ common.mk
@@ -66,7 +66,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5-service_64 \
     libsensorndkbridge \
-    Snap
+    GCameraGoPrebuilt
 
 # ConfigStore
 PRODUCT_PACKAGES += \
