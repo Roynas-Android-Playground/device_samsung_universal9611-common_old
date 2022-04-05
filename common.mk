fa4116e5e02beceeaef27d7bc7ffb17e7bb07ce7
santhosh6194 <santhosh6194@gmail.com>
2022-04-05 01:43:47 -0400

    a50-common: Switch to Samsung USB 1.3 HAL
    Signed-off-by: santhosh6194 <santhosh6194@gmail.com>

--- common.mk
+++ common.mk
@@ -253,7 +253,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1-service.typec
+    android.hardware.usb@1.3-service.samsung
 
 # Vibrator
 PRODUCT_PACKAGES += \
