c0b036477a45eb8e84e73d07f6e912f51ba506d9
roynatech2544 <whiteshell2544@naver.com>
2023-01-22 19:41:51 +0900

    universal9611: Declare dtb.img is in boot.img

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -35,7 +35,6 @@ def PrintInfo(info, dest):
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
 
 def OTA_InstallEnd(info):
-  AddImage(info, "RADIO", "dtb.img", "/dev/block/by-name/dtb")
   AddImage(info, "IMAGES", "dtbo.img", "/dev/block/by-name/dtbo")
   AddImage(info, "IMAGES", "vbmeta.img", "/dev/block/by-name/vbmeta")
   return
