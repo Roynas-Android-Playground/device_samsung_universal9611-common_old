2379f981b450aab827968fa55f722076d104f098
roynatech2544 <whiteshell2544@naver.com>
2023-01-23 13:43:27 +0900

    universal9611: shims: libaudioproxy: Clang-format src

--- shims/libaudioproxy/shim.cpp
+++ shims/libaudioproxy/shim.cpp
@@ -15,10 +15,10 @@
  */
 
 #include <errno.h>
-#include <unistd.h>
 #include <sound/asound.h>
+#include <unistd.h>
 
-extern "C" int get_dai_link(int /*param_1*/,int /*param_2*/) {
+extern "C" int get_dai_link(int /*param_1*/, int /*param_2*/) {
   /*if (param_1 != 0) {
     __android_log_print(2,"audio_route","requested PCM for %d",param_2);
   }*/
@@ -36,44 +36,46 @@ extern "C" int audio_route_missing_ctl(int *param_1) {
 }
 
 struct mixer {
-    int fd;
-    struct snd_ctl_card_info card_info;
-    struct snd_ctl_elem_info *elem_info;
-    struct mixer_ctl *ctl;
-    unsigned int count;
+  int fd;
+  struct snd_ctl_card_info card_info;
+  struct snd_ctl_elem_info *elem_info;
+  struct mixer_ctl *ctl;
+  unsigned int count;
 };
 
 struct audio_route {
-    struct mixer *mixer;
-    unsigned int num_mixer_ctls;
-    struct mixer_state *mixer_state;
+  struct mixer *mixer;
+  unsigned int num_mixer_ctls;
+  struct mixer_state *mixer_state;
 
-    unsigned int mixer_path_size;
-    unsigned int num_mixer_paths;
-    struct mixer_path *mixer_path;
+  unsigned int mixer_path_size;
+  unsigned int num_mixer_paths;
+  struct mixer_path *mixer_path;
 };
 
-extern "C" int * mixer_read_event(struct mixer *mixer, uint param_2) {
-    struct snd_ctl_event *ev = NULL;
-    ssize_t count;
+extern "C" int *mixer_read_event(struct mixer *mixer, uint param_2) {
+  struct snd_ctl_event *ev = NULL;
+  ssize_t count;
 
-    if (mixer && mixer->fd >= 0) {
-        ev = (struct snd_ctl_event *)calloc(1, sizeof(*ev));
+  if (mixer && mixer->fd >= 0) {
+    ev = (struct snd_ctl_event *)calloc(1, sizeof(*ev));
 
-        if (ev) {
-            count = read(mixer->fd, ev, sizeof(*ev));
+    if (ev) {
+      count = read(mixer->fd, ev, sizeof(*ev));
 
-            while (0 < count) {
-                if (ev->type == 0 && (ev->data.elem.mask & param_2) != 0) {
-                    return (int *)ev;
-                }
-
-                count = read(mixer->fd, ev, sizeof(*ev));
-            }
-            free(ev);
+      while (0 < count) {
+        if (ev->type == 0 && (ev->data.elem.mask & param_2) != 0) {
+          return (int *)ev;
         }
-    }
 
-    return NULL;
+        count = read(mixer->fd, ev, sizeof(*ev));
+      }
+      free(ev);
+    }
+  }
+
+  return NULL;
 }
-extern "C" int * mixer_read_event_sec(struct mixer *mixer, uint param_2) {return mixer_read_event(mixer, param_2);}
+extern "C" int *mixer_read_event_sec(struct mixer *mixer, uint param_2) {
+  return mixer_read_event(mixer, param_2);
+}
