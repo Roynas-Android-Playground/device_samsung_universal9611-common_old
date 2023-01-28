66c1237a966791c545f2867ff591b20504b517a3
roynatech2544 <whiteshell2544@naver.com>
2023-01-28 09:59:59 +0900

    universal9611: releasetools: Describe OneUI5 base fw requirement

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -48,7 +48,7 @@ def OTA_Assertions(info):
   m = re.search(r'require\s+version-bootloader-min\s*=\s*(\S+)', android_info)
   if m:
     bootloader_version = m.group(1)
-    cmd = ('assert(exynos9611.verify_bootloader_min("{}") == "1" || abort("ERROR: This package requires Android 10 or 11 based firmware. Please upgrade firmware and retry!"););').format(bootloader_version)
+    cmd = ('assert(exynos9611.verify_bootloader_min("{}") == "1" || abort("ERROR: This package requires OneUI 5 based firmware. Please upgrade firmware and retry!"););').format(bootloader_version)
     info.script.AppendExtra(cmd)
   return
 
