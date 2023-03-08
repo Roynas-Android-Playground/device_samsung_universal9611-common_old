e3eed7a95f38f3489559852a02466afe9590a016
Michael Benedict <michaelbt@live.com>
2023-03-08 14:45:39 +0900

    universal9611: fod: set fod rect
    Signed-off-by: Michael Benedict <michaelbt@live.com>
    Change-Id: I0936bbc17c7a6837f393e0795097246802d475e0

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -100,6 +100,7 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr) {
 #endif
 
     set(TSP_CMD_PATH, "fod_enable,1,1,0");
+    set(TSP_CMD_PATH, "set_fod_rect,440,2020,640,2220");
 }
 
 BiometricsFingerprint::~BiometricsFingerprint() {
