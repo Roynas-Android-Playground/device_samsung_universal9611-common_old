ba28cae4b5860dc8e90076f2e1f811c7bcf3e224
roynatech2544 <whiteshell2544@naver.com>
2023-03-15 11:04:56 +0900

    universal9611: BoardConfig: Do not include exynos sepolicy
    Currently, this sepolicy rules included is only Lineage livedisplay HAL, we do not use it. Although it can be added back in the future, lets remove for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -144,7 +144,6 @@ VENDOR_SECURITY_PATCH := 2022-10-01
 
 ## SELinux
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
-include device/lineage/sepolicy/exynos/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
