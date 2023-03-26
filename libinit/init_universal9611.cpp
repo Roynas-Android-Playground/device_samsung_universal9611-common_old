6693692a85644c3dcab5b8e6870bff778d789118
roynatech2544 <whiteshell2544@naver.com>
2023-03-26 15:08:06 +0900

    universal9611: libinit: Override models according to bootloader cmdline

--- libinit/init_universal9611.cpp
+++ libinit/init_universal9611.cpp
@@ -5,9 +5,22 @@
  */
 
 #include <libinit_dalvik_heap.h>
-
+#include <libinit_utils.h>
 #include "vendor_init.h"
 
+#include <android-base/properties.h>
+
+using android::base::GetProperty;
+
 void vendor_load_properties() {
+    std::string model;
+
+    model = GetProperty("ro.boot.product.model", "");
+    if (model.empty()) {
+       model = GetProperty("ro.boot.em.model", "");
+    }
+    set_ro_build_prop("model", model, true);
+    set_ro_build_prop("product", model, false);
+
     set_dalvik_heap();
 }
