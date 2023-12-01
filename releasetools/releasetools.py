d6e9baadf42c3c8fc32b360b7fc4a1e28c3e7b51
roynatech2544 <whiteshell2544@naver.com>
2023-12-01 12:16:29 +0900

    universal9611: Move A51 specific bits to a51 tree

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -17,16 +17,8 @@
 import common
 import re
 
-def FullOTA_Assertions(info):
-  OTA_Assertions(info)
-  return
-
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  return
-
-def IncrementalOTA_Assertions(info):
-  OTA_Assertions(info)
   return
 
 def IncrementalOTA_InstallEnd(info):
@@ -39,18 +31,6 @@ def AddImage(info, dir, basename, dest):
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
-
-def PrintInfo(info, dest):
-  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-
-def OTA_Assertions(info):
-  android_info = info.input_zip.read("OTA/android-info.txt").decode('utf-8')
-  m = re.search(r'require\s+version-bootloader-min\s*=\s*(\S+)', android_info)
-  if m:
-    bootloader_version = m.group(1)
-    cmd = ('assert(exynos9611.verify_bootloader_min("{}") == "1" || abort("ERROR: This package requires OneUI 5 based firmware. Please upgrade firmware and retry!"););').format(bootloader_version)
-    info.script.AppendExtra(cmd)
-  return
 
 def OTA_InstallEnd(info):
   AddImage(info, "IMAGES", "dtbo.img", "/dev/block/by-name/dtbo")
